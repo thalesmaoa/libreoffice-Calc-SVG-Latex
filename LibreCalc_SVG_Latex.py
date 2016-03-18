a4b606a80ebd459823cb56011e5c423de43d9f39
thalesmaoa <thalesmaia@gmail.com>
2016-03-18 17:43:49 -0300

    Update LibreCalc_SVG_Latex.py
    Uncommented

--- LibreCalc_SVG_Latex.py
+++ LibreCalc_SVG_Latex.py
@@ -42,29 +42,6 @@ def main():
             else:
                 print "Coudn't find x,y coordinates"
                 
-    #        print child.tag
-    #        print child[0].keys()
-    #        # x,y coordinates are in tspan class
-    #        if 'x' in child[0][0].keys():
-    #            print 'Tem x'
-    #        else:
-    #            print 'Nao tem x'
-        
-        
-    #    # If tag is text    
-    #    if child.tag[-4:] == 'text':
-    #        # Get the first tspan x, y info
-    #        # <text><tspan><tspan><tspan>
-    #        # <\tspan><\tspan>\<tspan><\text>
-    #        if child[0].tag[-5:] == 'tspan':        
-    #            x_str = child[0][0].attrib['x']
-    #            y_str = child[0][0].attrib['y']
-    #        
-    #            # Add coordinate do text also
-    #            child[0].set('x', x_str)
-    #            child[0].set('y', y_str)
-    #            child[0].set('co', 'co')
-    
     tree.write('aux.svg')
     subprocess.call(['/usr/bin/inkscape', '-D', '-z',  '--file=aux.svg', '--export-pdf=' + svg_file[:-3] + 'pdf', '--export-latex'])
     subprocess.call(['rm', 'aux.svg'])
